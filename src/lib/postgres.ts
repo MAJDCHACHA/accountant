56e0beadf057da18ecd625b52de76d6b0cd4d6c2
MAJD CHACHA <mastersshasha@gmail.com>
2025-07-03 17:58:37 +0300

    initial commit

--- src/lib/postgres.ts
+++ src/lib/postgres.ts
@@ -17,11 +17,11 @@ export const AppDataSource = new DataSource({
   synchronize: true, // استخدم false في الإنتاج
   logging: false,
   entities: [User, Account, JournalEntry, JournalDetails,AccountRelation],
-  // extra: {
-  //   ssl: {
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export const connectToDatabase = async (): Promise<void> => {
